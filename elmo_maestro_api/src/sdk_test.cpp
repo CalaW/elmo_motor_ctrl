f4ac94933e80171e7b452cd6a6b09c00d03fe29e
Chen Chen <maker_cc@foxmail.com>
2024-01-05 21:16:47 +0800

    restructure repo

---
+++ elmo_maestro_api/src/sdk_test.cpp
@@ -0,0 +1,10 @@
+#include <cstdio>
+
+int main(int argc, char ** argv)
+{
+  (void) argc;
+  (void) argv;
+
+  printf("hello world elmo_maestro_api package\n");
+  return 0;
+}
---
+++ elmo_maestro_hardware/src/elmo_maestro_hardware.cpp
@@ -0,0 +1,10 @@
+#include <cstdio>
+
+int main(int argc, char ** argv)
+{
+  (void) argc;
+  (void) argv;
+
+  printf("hello world elmo_maestro_hardware package\n");
+  return 0;
+}
