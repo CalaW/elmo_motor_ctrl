11a7697ad0d1839d58dd124a784546f0d69d5fc3
Chen Chen <maker_cc@foxmail.com>
2024-01-30 21:14:38 +0800

    wip ros2_control

---
+++ elmo_maestro_hardware/include/elmo_maestro_hardware/elmo_maestro_hi.hpp
@@ -0,0 +1,89 @@
+#ifndef ELMO_MAESTRO_HARDWARE__ELMO_MAESTRO_HI_HPP_
+#define ELMO_MAESTRO_HARDWARE__ELMO_MAESTRO_HI_HPP_
+
+#include <array>
+
+#include "rclcpp/macros.hpp"
+#include "rclcpp/rclcpp.hpp"
+#include "hardware_interface/system_interface.hpp"
+#include "elmo_maestro_hardware/visibility_control.h"
+
+#define ENDIAN_SELECT 123
+#define OS_PLATFORM 777
+#include "elmo_maestro_api/OS_PlatformDependSetting.h"
+#include "elmo_maestro_api/OS_PlatformDataTypeSizeSet.h"
+#include "elmo_maestro_api/OS_PlatformLinuxRpc64.hpp"
+#include "elmo_maestro_api/MMC_definitions.h"
+#include "elmo_maestro_api/MMCPPlib.hpp"
+
+namespace elmo_maestro_hardware
+{
+class ElmoMaestroHardwareInterface : public hardware_interface::SystemInterface
+{
+public:
+  RCLCPP_SHARED_PTR_DEFINITIONS(ElmoMaestroHardwareInterface);
+
+  ELMO_MAESTRO_HARDWARE_PUBLIC
+  ElmoMaestroHardwareInterface();
+
+  // initialize all variables and process parameters
+  ELMO_MAESTRO_HARDWARE_PUBLIC
+  hardware_interface::CallbackReturn on_init(
+    const hardware_interface::HardwareInfo & info) override;
+
+  // setup communication with the hardware
+  ELMO_MAESTRO_HARDWARE_PUBLIC
+  hardware_interface::CallbackReturn on_configure(
+    const rclcpp_lifecycle::State & previous_state) override;
+
+  // cleanup communication with the hardware
+  ELMO_MAESTRO_HARDWARE_PUBLIC
+  hardware_interface::CallbackReturn on_cleanup(
+    const rclcpp_lifecycle::State & previous_state) override;
+
+  // define state resources
+  ELMO_MAESTRO_HARDWARE_PUBLIC
+  std::vector<hardware_interface::StateInterface> export_state_interfaces() override;
+
+  // define command resources
+  ELMO_MAESTRO_HARDWARE_PUBLIC
+  std::vector<hardware_interface::CommandInterface> export_command_interfaces() override;
+
+  // enable "power"
+  ELMO_MAESTRO_HARDWARE_PUBLIC
+  hardware_interface::CallbackReturn on_activate(
+    const rclcpp_lifecycle::State & previous_state) override;
+
+  // stop all movement
+  ELMO_MAESTRO_HARDWARE_PUBLIC
+  hardware_interface::CallbackReturn on_deactivate(
+    const rclcpp_lifecycle::State & previous_state) override;
+
+  // 
+  ELMO_MAESTRO_HARDWARE_PUBLIC
+  hardware_interface::CallbackReturn on_shutdown(
+    const rclcpp_lifecycle::State & previous_state) override;
+
+  ELMO_MAESTRO_HARDWARE_PUBLIC
+  hardware_interface::CallbackReturn on_error(
+    const rclcpp_lifecycle::State & previous_state) override;
+
+  ELMO_MAESTRO_HARDWARE_PUBLIC
+  hardware_interface::return_type read(
+    const rclcpp::Time & time, const rclcpp::Duration & period) override;
+
+  ELMO_MAESTRO_HARDWARE_PUBLIC
+  hardware_interface::return_type write(
+    const rclcpp::Time & time, const rclcpp::Duration & period) override;
+
+private:
+  std::array<CMMCSingleAxis, 1> axes_;
+  MMC_CONNECTION_PARAM_STRUCT conn_param_;
+  MMC_CONNECT_HNDL conn_hndl_;
+  CMMCConnection conn_;
+  void elmoInit();
+  void onEmergency(unsigned short usAxisRef, short sEmcyCode);
+};
+}  // namespace elmo_maestro_hardware
+
+#endif  // ELMO_MAESTRO_HARDWARE_ELMO_MAESTRO_HW_HPP_
---
+++ elmo_maestro_hardware/src/elmo_maestro_hi.cpp
@@ -0,0 +1,149 @@
+#include "elmo_maestro_hardware/elmo_maestro_hi.hpp"
+
+using CallbackReturn = rclcpp_lifecycle::node_interfaces::LifecycleNodeInterface::CallbackReturn;
+
+namespace elmo_maestro_hardware
+{
+
+// initialize all variables and process parameters
+hardware_interface::CallbackReturn ElmoMaestroHardwareInterface::on_init(
+  const hardware_interface::HardwareInfo & info)
+{
+  if (hardware_interface::SystemInterface::on_init(info) != CallbackReturn::SUCCESS) {
+#pragma push_macro("ERROR")
+#undef ERROR
+    return CallbackReturn::ERROR;
+#pragma pop_macro("ERROR")
+  }
+}
+
+// setup communication with the hardware
+hardware_interface::CallbackReturn ElmoMaestroHardwareInterface::on_configure(
+  const rclcpp_lifecycle::State & previous_state)
+{
+  return CallbackReturn::SUCCESS;
+}
+
+// cleanup communication with the hardware
+hardware_interface::CallbackReturn ElmoMaestroHardwareInterface::on_cleanup(
+  const rclcpp_lifecycle::State & previous_state)
+{
+  return CallbackReturn::SUCCESS;
+}
+
+// define state resources
+std::vector<hardware_interface::StateInterface>
+ElmoMaestroHardwareInterface::export_state_interfaces()
+{
+}
+
+// define command resources
+std::vector<hardware_interface::CommandInterface>
+ElmoMaestroHardwareInterface::export_command_interfaces()
+{
+}
+
+// enable "power"
+hardware_interface::CallbackReturn ElmoMaestroHardwareInterface::on_activate(
+  const rclcpp_lifecycle::State & previous_state)
+{
+  return CallbackReturn::SUCCESS;
+}
+
+// stop all movement
+hardware_interface::CallbackReturn ElmoMaestroHardwareInterface::on_deactivate(
+  const rclcpp_lifecycle::State & previous_state)
+{
+  return CallbackReturn::SUCCESS;
+}
+
+//
+hardware_interface::CallbackReturn ElmoMaestroHardwareInterface::on_shutdown(
+  const rclcpp_lifecycle::State & previous_state)
+{
+  return CallbackReturn::SUCCESS;
+}
+
+ELMO_MAESTRO_HARDWARE_PUBLIC
+hardware_interface::CallbackReturn ElmoMaestroHardwareInterface::on_error(
+  const rclcpp_lifecycle::State & previous_state)
+{
+  return CallbackReturn::SUCCESS;
+}
+
+ELMO_MAESTRO_HARDWARE_PUBLIC
+hardware_interface::return_type ElmoMaestroHardwareInterface::read(
+  const rclcpp::Time & time, const rclcpp::Duration & period)
+{
+}
+
+ELMO_MAESTRO_HARDWARE_PUBLIC
+hardware_interface::return_type ElmoMaestroHardwareInterface::write(
+  const rclcpp::Time & time, const rclcpp::Duration & period)
+{
+}
+
+/**
+ * @brief Initialize the system, including axes, communication, etc.
+ * 
+ * @return int 
+ */
+void ElmoMaestroHardwareInterface::elmoInit()
+{
+  printf("init connection\n");
+
+  // this is not safe but have to do this
+  auto host_ip = (char *)"192.168.1.2";
+  auto target_ip = (char *)"192.168.1.3";
+
+  conn_param_.uiTcpPort = 4000;
+  strcpy((char *)conn_param_.ucIp, target_ip);
+
+  conn_hndl_ = conn_.ConnectRPCEx(host_ip, target_ip, 0x7fffffff, nullptr);
+
+  // Register the callback function for Emergency:
+  conn_.RegisterEventCallback(MMCPP_EMCY, nullptr);
+
+  // Set Try-Catch flag Enable\Disable
+  CMMCPPGlobal::Instance()->SetThrowFlag(false);
+  CMMCPPGlobal::Instance()->SetThrowWarningFlag(false);
+
+  try {
+    axes_[0].InitAxisData("a01", conn_hndl_);
+
+    for (auto & axis : axes_) {
+      auto status = axis.ReadStatus();
+
+      if (status & NC_AXIS_ERROR_STOP_MASK) {
+        std::cout << std::hex << status << " " << std::hex << NC_AXIS_STOPPING_MASK << std::endl;
+        axis.ResetAsync();
+        status = axis.ReadStatus();
+        if (status & NC_AXIS_ERROR_STOP_MASK) {
+          throw runtime_error(to_string(status));
+        }
+      }
+    }
+  } catch (CMMCException exp) {
+    cout << "init failed!!" << exp.what() << endl;
+    exit(1);
+  }
+  std::cout << std::endl;
+}
+
+/**
+ * @brief Call back function if emergency received
+ * 
+ * @param usAxisRef 
+ * @param sEmcyCode 
+ */
+void ElmoMaestroHardwareInterface::onEmergency(unsigned short usAxisRef, short sEmcyCode)
+{
+  printf("Emergency Message Received on Axis %d. Code: %x\n", usAxisRef, sEmcyCode);
+}
+
+}  // namespace elmo_maestro_hardware
+
+#include "pluginlib/class_list_macros.hpp"
+
+PLUGINLIB_EXPORT_CLASS(
+  elmo_maestro_hardware::ElmoMaestroHardwareInterface, hardware_interface::SystemInterface)
